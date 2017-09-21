fabefb4fde78fc05c001f0804e5fc6fdd24a7955
Jackie Chui <jshchui@gmail.com>
2017-09-20 22:45:00 -0700

    displays X and Os when placed, prevents input when data is already there. CHeck win condition partially works

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 document.addEventListener("DOMContentLoaded", function(){
 
-  turn = 0;
+  let turn = 'O';
+
+  let XMoves = [];
+  let OMoves = [];
 
   let playField = [
     [], [], [],
@@ -8,21 +11,62 @@ document.addEventListener("DOMContentLoaded", function(){
     [], [], []
   ];
 
-  console.log('js ready');
+  let winConditions = ['0', '1', '2'];
 
-  // const checkBox = document.getElementsByClassName("checkBox");
-  // const checkBox = document.getElementById("wtf");
-  //
-  // checkBox.addEventListener('click', function show() {
-  //   var boxData = data.getAttribute("data-box");
-  //
-  //   console.log(boxData);
-  // })
+  console.log('js ready');
 
   window.showDetails = function(data) {
     var boxData = data.getAttribute("data-box");
-    console.log(boxData);
+
+    console.log(playField[boxData].length);
+    if(playField[boxData].length === 0) {
+
+      playField[boxData].push(boxData);
+
+      console.log(playField[boxData]);
+      console.log('inputing');
+      if(turn == 'X') {
+        XMoves.push(boxData);
+        turn = '0';
+      } else {
+        OMoves.push(boxData);
+        turn = 'X';
+      }
+      document.getElementById(`${boxData}`).innerHTML = `${turn}`;
+      document.getElementById('title').innerHTML = `${turn}'s Turn`;
+    }
+    checkWin()
+    console.log('X', XMoves);
+    console.log('O', OMoves);
+    // console.log(playField);
+  }
+
+  function containsAll(winCond, haystack){
+    // for(var i = 0 , len = winCond.length; i < len; i++){
+      let winner = true;
+       winCond.map((winCondNum) => {
+         if(haystack.includes(winCondNum)) {
+
+         } else {
+           winner = false;
+         }
+       })
+    // }
+    console.log(winner);
+    return winner;
+  }
+
+  // containsAll([34, 78, 89], [78, 67, 34, 99, 56, 89]); // true
+  // containsAll([34, 78, 89], [78, 67, 99, 56, 89]); // false
+  // containsAll([34, 78, 89], [78, 89]); // false
+  // false
 
 
+  function checkWin() {
+    if(turn == 'X') {
+      containsAll(winConditions, XMoves);
+    } else {
+      containsAll(winConditions, OMoves);
+    }
   }
 });
