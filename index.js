927657a21d7eddaf34c426aff0b51daca95e7d5a
Jackie Chui <jshchui@gmail.com>
2017-09-21 18:15:55 -0700

    winning condition has been implementd

--- index.js
+++ index.js
@@ -11,62 +11,76 @@ document.addEventListener("DOMContentLoaded", function(){
     [], [], []
   ];
 
-  let winConditions = ['0', '1', '2'];
-
-  console.log('js ready');
+  let winConditions = [
+    ['0', '1', '2'],
+    ['3', '4', '5'],
+    ['6', '7', '8'],
+    ['0', '3', '6'],
+    ['1', '4', '7'],
+    ['2', '5', '8'],
+    ['0', '4', '8'],
+    ['2', '4', '6']
+  ];
 
   window.showDetails = function(data) {
     var boxData = data.getAttribute("data-box");
-
-    console.log(playField[boxData].length);
     if(playField[boxData].length === 0) {
 
       playField[boxData].push(boxData);
 
-      console.log(playField[boxData]);
-      console.log('inputing');
+      document.getElementById(`${boxData}`).innerHTML = `${turn}`;
       if(turn == 'X') {
         XMoves.push(boxData);
-        turn = '0';
+        checkWin()
+
+        turn = 'O';
       } else {
         OMoves.push(boxData);
+        checkWin()
+
         turn = 'X';
       }
-      document.getElementById(`${boxData}`).innerHTML = `${turn}`;
       document.getElementById('title').innerHTML = `${turn}'s Turn`;
     }
-    checkWin()
     console.log('X', XMoves);
     console.log('O', OMoves);
     // console.log(playField);
   }
 
-  function containsAll(winCond, haystack){
-    // for(var i = 0 , len = winCond.length; i < len; i++){
+  function containsAll(winCond, playerMoves){
+    return winCond.map((winningConditions) => { //Loops through all winning conditions
       let winner = true;
-       winCond.map((winCondNum) => {
-         if(haystack.includes(winCondNum)) {
 
-         } else {
-           winner = false;
-         }
-       })
+      winningConditions.map((winCondNum) => { //loops through all winning condition numbers
+        if(playerMoves.includes(winCondNum)) {
+
+        } else {
+          winner = false;
+        }
+      })
+
+      if(winner) {
+        return console.log(`${turn} has won the game`);
+      } else {
+        return console.log(`${turn} has gone, no win`);
+
+      }
+    })
+    // for(var i = 0 ; i < winCond.length; i++){
     // }
-    console.log(winner);
-    return winner;
+
+    // if(winner) {
+    //   return console.log(`${turn} has won the game`);
+    // } else {
+    //   return console.log(`${turn} has gone, no win`);
+    // }
   }
-
-  // containsAll([34, 78, 89], [78, 67, 34, 99, 56, 89]); // true
-  // containsAll([34, 78, 89], [78, 67, 99, 56, 89]); // false
-  // containsAll([34, 78, 89], [78, 89]); // false
-  // false
-
 
   function checkWin() {
     if(turn == 'X') {
-      containsAll(winConditions, XMoves);
+      return containsAll(winConditions, XMoves);
     } else {
-      containsAll(winConditions, OMoves);
+      return containsAll(winConditions, OMoves);
     }
   }
 });
